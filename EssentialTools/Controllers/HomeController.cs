af5eab30f35bda0c63d517fb4dd314ce30c2d16f
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-27 10:21:04 -0400

    Add Ninject to HomeController.

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EssentialTools.Models;
+using Ninject;
 
 namespace EssentialTools.Controllers
 {
@@ -19,9 +20,15 @@ namespace EssentialTools.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            LinqValueCalculator calc = new LinqValueCalculator();
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+
             decimal totalValue = cart.CalculateProductTotal();
+
             return View(totalValue);
         }
     }
